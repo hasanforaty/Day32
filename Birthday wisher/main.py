8558c57bddbc8ffde99afe712aa1307e7c13c0b9
hasan foraty <hasanforaty19@gmail.com>
2024-05-12 09:23:23 +0330

    complete birthday smtp project

--- Birthday wisher/main.py
+++ Birthday wisher/main.py
@@ -1,22 +1,28 @@
-##################### Hard Starting Project ######################
+import pandas
+import datetime as dt
+import random
+import smtplib
 
-# 1. Update the birthdays.csv with your friends & family's details. 
-# HINT: Make sure one of the entries matches today's date for testing purposes. 
-
-# 2. Check if today matches a birthday in the birthdays.csv
-# HINT 1: Only the month and day matter. 
-# HINT 2: You could create a dictionary from birthdays.csv that looks like this:
-# birthdays_dict = {
-#     (month, day): data_row
-# }
-#HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
-# if (today_month, today_day) in birthdays_dict:
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT: https://www.w3schools.com/python/ref_string_replace.asp
-
-# 4. Send the letter generated in step 3 to that person's email address.
-# HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
-
-
-
+password = 'gkoo jziv yvvm prcs'
+provider = 'foratyhasan24@gmail.com'
+now = dt.datetime.now()
+births = (pandas.read_csv('birthdays.csv', )
+          .query('month == @now.month and day == @now.day')
+          .to_dict(orient="records"))
+for birth in births:
+    letter_name = f'letter_{random.choice(range(1, 4))}'
+    with open(f'./letter_templates/{letter_name}.txt', 'r') as letter:
+        name = birth.get('name')
+        letter_template = letter.read()
+        letter_template = letter_template.replace('[NAME]', name)
+        header = f'Happy birthday {name}'
+        email = birth.get('email')
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
+            server.starttls()
+            server.login(user=provider, password=password)
+            print(f'sending email to {name} ......')
+            server.sendmail(
+                from_addr=provider,
+                to_addrs=email,
+                msg=f'Subject:{header}\n\n{letter_template}\n'
+            )
